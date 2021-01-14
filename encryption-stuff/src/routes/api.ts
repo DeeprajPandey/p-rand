2b123a09dac068862555d1278c1e22ab7c323d13
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-01-14 12:16:57 +0530

    Refactor routes: add hash and verify routes for bcrypt

--- encryption-stuff/src/routes/api.ts
+++ encryption-stuff/src/routes/api.ts
@@ -1,27 +1,41 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcryptjs';
 import Joi from 'joi';
 
 const api = Router();
 
-const passSchema = Joi.object().keys({
-  payload: Joi.string()
-    .required()
-    .min(8)
-    .pattern(new RegExp('^[a-zA-Z0-9]{8,30}$'))
+const hashSchema = Joi.object().keys({
+  payload: Joi.string().required().min(8).pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')),
 });
 
-api.route('/v1/bcrypt')
-.post(async (req: Request, res: Response, next) => {
-    passSchema.validateAsync(req.body)
+const verifySchema = Joi.object().keys({
+  hash: Joi.string().required(),
+  plaintextPass: Joi.string().required().min(8).pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')),
+});
+
+api.post('/v1/hash/bcrypt', async (req: Request, res: Response, next: NextFunction) => {
+  hashSchema
+    .validateAsync(req.body)
     .then((r) => {
-      bcrypt.hash(r.payload, 11)
-      .then((hashed) => {
+      bcrypt.hash(r.payload, 11).then((hashed) => {
         res.status(200).json({ hashed });
-      })
+      });
     })
     .catch(next);
-
+});
+api.post('/v1/verify/bcrypt', async (req: Request, res: Response, next: NextFunction) => {
+  verifySchema
+    .validateAsync(req.body)
+    .then((r) => {
+      bcrypt.compare(r.plaintextPass, r.hash).then((match) => {
+        if (match) {
+          res.status(200).json({ status: 'match' });
+        } else {
+          res.status(400).json({ status: 'fail' });
+        }
+      }); // end of bcrypt compare
+    })
+    .catch(next);
 });
 
 export default api;
