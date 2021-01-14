2ed0dbbf0edcbc07f1faceec0f6bfd5697a131c5
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-01-14 11:22:37 +0530

    Update route to bcrypt

--- encryption-stuff/src/routes/api.ts
+++ encryption-stuff/src/routes/api.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import Joi from 'joi';
 
 const api = Router();
@@ -11,11 +11,14 @@ const passSchema = Joi.object().keys({
     .pattern(new RegExp('^[a-zA-Z0-9]{8,30}$'))
 });
 
-api.route('/v1/encrypt')
+api.route('/v1/bcrypt')
 .post(async (req: Request, res: Response, next) => {
     passSchema.validateAsync(req.body)
-    .then(() => {
-      res.status(200).json({ 'response': '☕️ huehuehue' });
+    .then((r) => {
+      bcrypt.hash(r.payload, 11)
+      .then((hashed) => {
+        res.status(200).json({ hashed });
+      })
     })
     .catch(next);
 
