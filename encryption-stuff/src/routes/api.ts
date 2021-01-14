0adac58d304d271913646b8385a64c100449f027
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-01-14 16:36:22 +0530

    Add endpoint for salted sha256

--- encryption-stuff/src/routes/api.ts
+++ encryption-stuff/src/routes/api.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcryptjs';
 import argon2 from 'argon2';
+import sha256 from 'crypto-js/sha256';
+import Base64 from 'crypto-js/enc-base64';
 import Joi from 'joi';
-import bodyParser from 'body-parser';
 
 const api = Router();
 
@@ -12,6 +13,12 @@ const hashSchema = Joi.object().keys({
 
 const verifySchema = Joi.object().keys({
   hash: Joi.string().required(),
+  plaintextPass: Joi.string().required().min(8).pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')),
+});
+
+const verifySaltedSchema = Joi.object().keys({
+  hash: Joi.string().required(),
+  salt: Joi.string().required(),
   plaintextPass: Joi.string().required().min(8).pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')),
 });
 
@@ -64,6 +71,32 @@ api.post('/v1/verify/argon2', async (req: Request, res: Response, next: NextFunc
           res.status(400).json({ status: 'fail' });
         }
       }); // end of argon2 verify
+    })
+    .catch(next);
+});
+
+// sha256
+api.post('/v1/hash/saltedsha256', async (req: Request, res: Response, next: NextFunction) => {
+  hashSchema
+    .validateAsync(req.body)
+    .then((r) => {
+      const salt = Date.now();
+      const hashed = sha256(salt + r.payload).toString(Base64);
+      res.status(200).send({ salt, hashed });
+    })
+    .catch(next);
+});
+
+api.post('/v1/verify/saltedsha256', async (req: Request, res: Response, next: NextFunction) => {
+  verifySaltedSchema
+    .validateAsync(req.body)
+    .then((r) => {
+      const hashed = sha256(r.salt + r.plaintextPass).toString(Base64);
+      if (hashed == r.hash) {
+        res.status(200).json({ status: 'match' });
+      } else {
+        res.status(400).json({ status: 'fail' });
+      }
     })
     .catch(next);
 });
