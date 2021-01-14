2732c10566aae4541cb13ece000c116dc6c0b778
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-01-14 16:08:23 +0530

    Add endpoint for argon2

--- encryption-stuff/src/routes/api.ts
+++ encryption-stuff/src/routes/api.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcryptjs';
+import argon2 from 'argon2';
 import Joi from 'joi';
+import bodyParser from 'body-parser';
 
 const api = Router();
 
@@ -13,6 +15,7 @@ const verifySchema = Joi.object().keys({
   plaintextPass: Joi.string().required().min(8).pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')),
 });
 
+// bcrypt
 api.post('/v1/hash/bcrypt', async (req: Request, res: Response, next: NextFunction) => {
   hashSchema
     .validateAsync(req.body)
@@ -34,6 +37,33 @@ api.post('/v1/verify/bcrypt', async (req: Request, res: Response, next: NextFunc
           res.status(400).json({ status: 'fail' });
         }
       }); // end of bcrypt compare
+    })
+    .catch(next);
+});
+
+// argon2
+api.post('/v1/hash/argon2', async (req: Request, res: Response, next: NextFunction) => {
+  hashSchema
+    .validateAsync(req.body)
+    .then((r) => {
+      argon2.hash(r.payload).then((hashed) => {
+        res.status(200).send({ hashed });
+      });
+    })
+    .catch(next);
+});
+
+api.post('/v1/verify/argon2', async (req: Request, res: Response, next: NextFunction) => {
+  verifySchema
+    .validateAsync(req.body)
+    .then((r) => {
+      argon2.verify(r.hash, r.plaintextPass).then((match: boolean) => {
+        if (match) {
+          res.status(200).json({ status: 'match' });
+        } else {
+          res.status(400).json({ status: 'fail' });
+        }
+      }); // end of argon2 verify
     })
     .catch(next);
 });
