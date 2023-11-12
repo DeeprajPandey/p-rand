4bb67570b19edeffb7fb93925318077147bfae40
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2023-11-12 12:01:50 +0530

    Test API usage on user-end

--- ts-signals/src/index.ts
+++ ts-signals/src/index.ts
@@ -4,15 +4,28 @@ function testSignals(): void {
     const {getter: points, setter: setPoints, sub, unsub} = createSignal(42);
 
     const logger = createSubscriber(() => {
-        console.log(points());
+        console.log(`\n>> Received notification: ${points()}\n`);
     });
 
-    
+
     // TEMP: should print 42
     console.log(points());
     
+    if (sub(logger)) {
+        console.info("Logger sub'd to points");
+    } else {
+        console.error(`${logger} (logger) could not subscribe to (points) ${points}`);
+    }
+
+    // update to 47 and emit to subscribers, logger should print 47
     setPoints(points() + 5);
-    
+
+    if (unsub(logger)) {
+        console.info("Logger unsub'd from points");
+    } else {
+        console.error(`${logger} (logger) could not unsubscribe from (points) ${points}`);
+    }
+
     // TEMP: should print 47
     console.log(points());
 
